1951aa7436c4bc829b7c376371cdfe2f4596d8f3
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-09-10 04:50:31 +0800

    Update farmscript.js

--- farmscript.js
+++ farmscript.js
@@ -30,8 +30,8 @@ var counterLimitT2 = 64; //X amount of the counter limit for other crops
 var counterLimitT3 = 120; //X amount of the counter limit for other crops
 
 var ProfitT1 = 35; //Profit per harvest
-var ProfitT2 = 55; //Profit modifier for other crops
-var ProfitT3 = 121.05; //Profit modifier for other crops
+var ProfitT2 = 60; //Profit modifier for other crops
+var ProfitT3 = 135.05; //Profit modifier for other crops
 
 var coopProfit = 25; //Profit from the purchased coop
 
